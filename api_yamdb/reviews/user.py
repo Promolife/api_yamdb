17d74733dcdba3723f6760cd90646d09e3880abd
Александр Сердюков <alphapromolife@yandex.ru>
2023-03-06 19:35:05 +0300

    Небольшие правка оформления

--- api_yamdb/reviews/user.py
+++ api_yamdb/reviews/user.py
@@ -66,4 +66,3 @@ def post_save(sender, instance, created, **kwargs):
         confirmation_code = default_token_generator.make_token(instance)
         instance.confirmation_code = confirmation_code
         instance.save()
-
